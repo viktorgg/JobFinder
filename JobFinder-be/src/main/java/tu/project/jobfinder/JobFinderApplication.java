7886092f8d5ec3ed652e382076bbe949303aff60
ViktorGG <viktor.grgv47@gmail.com>
2021-01-16 13:09:52 +0200

    Vuetify

--- JobFinder-be/src/main/java/tu/project/jobfinder/JobFinderApplication.java
+++ JobFinder-be/src/main/java/tu/project/jobfinder/JobFinderApplication.java
@@ -9,8 +9,4 @@ public class JobFinderApplication {
     public static void main(String[] args) {
         SpringApplication.run(JobFinderApplication.class, args);
     }
-
-
 }
-
-
--- jobfinder-fe/src/main.js
+++ jobfinder-fe/src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -8,9 +9,17 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BootstrapVue, BTable } from 'bootstrap-vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faCheck)
+
 Vue.use(VueAxios, axios)
 Vue.use(BootstrapVue)
+Vue.use(Vuetify)
 Vue.component('b-table', BTable)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
@@ -19,5 +28,6 @@ new Vue({
   router,
   store,
   axios,
+  Vuetify,
   render: h => h(App)
 }).$mount('#app')
