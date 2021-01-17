cf473239b008944edbc4540dfc4ca7f99bd0da4f
ViktorGG <viktor.grgv47@gmail.com>
2021-01-17 09:56:58 +0200

    Removed vue file

--- jobfinder-fe/src/main.js
+++ jobfinder-fe/src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -9,17 +8,9 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BootstrapVue, BTable } from 'bootstrap-vue'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(faCheck)
-
 Vue.use(VueAxios, axios)
 Vue.use(BootstrapVue)
-Vue.use(Vuetify)
 Vue.component('b-table', BTable)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
@@ -28,6 +19,5 @@ new Vue({
   router,
   store,
   axios,
-  Vuetify,
   render: h => h(App)
 }).$mount('#app')
